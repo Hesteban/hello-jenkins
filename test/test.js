c0eb6a299838d68fcb6307c67050b329ce704daa
Hector <hector.esteban@gmail.com>
2015-03-31 07:35:38 -0700

    Change test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello me', function(done) {
-    request(app).get('/').expect('hello me', done);
+    request(app).get('/').expect('hello Hector', done);
   });
 });
